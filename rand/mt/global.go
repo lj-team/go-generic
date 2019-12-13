ae350fe4c7d2c7f3d616480f80a4b25635e22811
Mikhail Kirillov <mikkirillov@yandex.ru>
2019-12-13 10:39:26 +0300

    rand/mt

---
+++ rand/mt/global.go
@@ -0,0 +1,39 @@
+package mt
+
+import (
+	"time"
+)
+
+var (
+	defGen       *Mersenne
+	output       chan int64
+	GlobalReseed int
+)
+
+func init() {
+	defGen = New()
+	defGen.Seed(time.Now().UnixNano())
+
+	output = make(chan int64, 1024)
+
+	GlobalReseed = 1000000
+
+	go generator()
+}
+
+func Next() int64 {
+	return <-output
+}
+
+func generator() {
+
+	i := 0
+
+	for {
+		output <- defGen.Int63()
+		i = (i + 1) % GlobalReseed
+		if i == 0 {
+			defGen.Seed(time.Now().UnixNano())
+		}
+	}
+}
