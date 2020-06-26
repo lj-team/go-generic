4112642169e9468c73577b72bd283356d80026ec
Mikhail Kirillov <mikkirillov@yandex.ru>
2020-06-26 13:55:22 +0300

    remove not more used redis interface

--- db/redis/config.go
+++
@@ -1,8 +0,0 @@
-package redis
-
-type Config struct {
-	Addr     string   `json:"addr"`
-	Failover bool     `json:"failover"`
-	Cluster  string   `json:"cluster"`
-	Sentinel []string `json:"sentinel"`
-}
